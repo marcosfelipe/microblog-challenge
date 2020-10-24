08ef054b889612c8f7fb969a3ddb1c51307a8062
marcosfelipe <marcos@pontogestor.com>
2020-10-24 13:50:55 -0300

    add a test for logged in user

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test 'should get root as anonymous' do
+    get root_path
+    assert_response :success
+  end
+
+  test 'should get root as logged user' do
+    sign_in users(:one)
     get root_path
     assert_response :success
   end
