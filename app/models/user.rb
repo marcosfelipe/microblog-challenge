b755b570cfdb17e712325dcb4476d3f7814f2e26
marcosfelipe <marcos@pontogestor.com>
2020-10-26 17:51:38 -0300

    add new relantionship between users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_and_belongs_to_many :friends,
+                          class_name: 'User',
+                          join_table:  :friendships,
+                          foreign_key: :user_id,
+                          association_foreign_key: :friend_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_011745) do
+ActiveRecord::Schema.define(version: 2020_10_26_191051) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
