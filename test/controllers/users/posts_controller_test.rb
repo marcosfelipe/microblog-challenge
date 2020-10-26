fd606c38372acd4215485c1e63b00b844c704117
marcosfelipe <marcos@pontogestor.com>
2020-10-26 18:14:31 -0300

    fix missing namespace

--- test/controllers/users/posts_controller_test.rb
+++ test/controllers/users/posts_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PostsControllerTest < ActionDispatch::IntegrationTest
+class Users::PostsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get user_posts_path(users(:one)), as: :json
     assert_response :success
