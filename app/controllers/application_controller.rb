cc67896141d7cc91312f4d256292d226beea22d5
marcosfelipe <marcos@pontogestor.com>
2020-10-26 16:04:03 -0300

    fix the devise default sign_in redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(*)
+    root_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :username, :email, :password)
