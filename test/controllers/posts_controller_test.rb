a8ccba1805e22a8fb39d8237d08cbffc7cde4b1f
marcosfelipe <marcos@pontogestor.com>
2020-10-26 11:01:12 -0300

    add devise authentication to posts controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,12 +2,19 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-  setup { sign_in users(:one) }
 
   test 'should create a post' do
+    sign_in users(:one)
     assert_difference 'Post.count' do
       post posts_path, as: :json, params: { post: { content: 'new post!' } }
     end
     assert_response :success
+  end
+
+  test 'should not create a post as anonymus' do
+    assert_no_difference 'Post.count' do
+      post posts_path, as: :json, params: { post: { content: 'new post!' } }
+    end
+    assert_response :unauthorized
   end
 end
