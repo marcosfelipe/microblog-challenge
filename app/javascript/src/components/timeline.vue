1e72f5b9ddfa393cf971ca03701dbac8854d50b9
marcosfelipe <marcos@pontogestor.com>
2020-10-25 22:05:19 -0300

    add the timeline component

---
+++ app/javascript/src/components/timeline.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card
+  elevation="2"
+  class="my-3"
+  >
+    <v-card-text>
+      This is my text.
+      <div class="text-right">
+        <v-btn
+          icon
+          disabled
+        >
+          <v-icon>mdi-thumb-up</v-icon>
+        </v-btn>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
--- app/javascript/src/home.vue
+++ app/javascript/src/home.vue
@@ -1,12 +1,17 @@
 <template>
-  <new-post />
+  <div>
+    <new-post />
+    <timeline />
+  </div>
 </template>
 <script>
+import timeline from './components/timeline.vue'
 import newPost from './components/new_post.vue'
 
 export default {
   name: 'Home',
   components: {
+    timeline,
     newPost
   }
 }
