be57f8c74d15083b2631b05cda0de2d991c3fa4e
marcosfelipe <marcos@pontogestor.com>
2020-10-26 17:52:18 -0300

    add follow and unfollow actions on users page

--- app/javascript/src/components/user.vue
+++ app/javascript/src/components/user.vue
@@ -38,7 +38,8 @@
                     class="text-no-wrap"
             >
                 <div class="text-right">
-                    <v-btn text disabled>Follow</v-btn>
+                    <v-btn rounded outlined color="cyan" v-if="notFollowing(user.id)" @click="follow(user.id)">Follow</v-btn>
+                    <v-btn rounded outlined color="error" v-else @click="unfollow(user.id)">Unfollow</v-btn>
                 </div>
             </v-col>
         </v-row>
@@ -46,6 +47,34 @@
 </template>
 <script>
     export default {
-        props: ['user']
+        props: ['user'],
+        data: function (){
+            return {
+                friends: []
+            }
+        },
+        methods: {
+            notFollowing: function(userId) {
+                return !this.friends.includes(userId)
+            },
+            fetchMyFriends: function(){
+                this.$http.get('/friendships').then(response => {
+                    this.friends = response.data.map(friend => friend.id)
+                }).catch(response => this.friends = [])
+            },
+            follow: function(userId){
+                this.$http.post('/friendships', { friend_id: userId }).then(response => {
+                    this.fetchMyFriends()
+                })
+            },
+            unfollow: function(userId){
+                this.$http.delete('/friendships', { data: { friend_id: userId } }).then(response => {
+                    this.friends = response.data
+                })
+            }
+        },
+        created: function(){
+            this.fetchMyFriends()
+        }
     }
 </script>
