fa67ff66769de34a413fcaabb438afde99b744ce
marcosfelipe <marcos@pontogestor.com>
2020-10-25 22:30:29 -0300

    add the posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def create
+    post = current_user.posts.new(post_params)
+    if post.save
+      render json: post.to_json
+    else
+      render json: { errors: post.errors }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit :content
+  end
+end
