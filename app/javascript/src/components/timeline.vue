7ec841519b95cf8d4c7d6524cd473094fdca487c
marcosfelipe <marcos@pontogestor.com>
2020-10-27 20:24:29 -0300

    add the fake user avatar

--- app/javascript/src/components/timeline.vue
+++ app/javascript/src/components/timeline.vue
@@ -3,17 +3,30 @@
   elevation="2"
   class="my-3"
   >
-    <v-card-title>Posts:</v-card-title>
+    <v-card-title>Timeline</v-card-title>
     <v-divider ></v-divider>
     <div v-for="post in posts" :key="post.id">
+      <v-row>
+        <v-col cols="2" class="pa-5">
+          <v-avatar color="red">
+            <v-icon dark>
+              mdi-account-circle
+            </v-icon>
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <v-card-text>
+            <b>
+              <router-link :to="'/users/' + post.user.id + '/posts'">{{ post.user.username }}</router-link>
+            </b>
+            - <label class="text--secondary">{{ post.created_at }}</label>
+          </v-card-text>
+          <v-card-text>
+            {{ post.content }}
+          </v-card-text>
+        </v-col>
+      </v-row>
       <v-card-text>
-        <b>
-          <router-link :to="'/users/' + post.user.id + '/posts'">{{ post.user.username }}</router-link>
-        </b>
-        - <label class="text--secondary">{{ post.created_at }}</label>
-      </v-card-text>
-      <v-card-text>
-        {{ post.content }}
         <div class="text-right">
           <v-btn
                   icon
@@ -23,7 +36,7 @@
           </v-btn>
           <v-btn @click="destroyPost(post.id)"
                  v-if="currentUser(post.user)"
-                  icon
+                 icon
           >
             <v-icon>mdi-trash-can-outline</v-icon>
           </v-btn>
