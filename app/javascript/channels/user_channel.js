af234cfe6c65b5ff8cc9ad3898cfe5613660326b
marcosfelipe <marcos@pontogestor.com>
2020-10-27 15:29:53 -0300

    create the user channel for notifications

---
+++ app/javascript/channels/user_channel.js
@@ -0,0 +1,14 @@
+// import consumer from "./consumer"
+//
+// consumer.subscriptions.create("UserChannel", {
+//   connected() {
+//   },
+//
+//   // disconnected() {
+//   //   // Called when the subscription has been terminated by the server
+//   // },
+//
+//   received(data) {
+//       console.log(data)
+//   }
+// });
